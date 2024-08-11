a1555c99894bca434ca88acbb7dfa0b311f4fecd
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-12 02:39:15 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<CheckList />
+			<Stats />
+		</div>
+	);
+}
+
+function Logo() {
+	return <span className="logo">📝 GoCheck ✅</span>;
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>Apa yang ingin Kamu catat?</h3>
+		</div>
+	);
+}
+
+function CheckList() {
+	return (
+		<div className="list">
+			<ul>
+				<li>Makan</li>
+				<li>Tidur</li>
+			</ul>
+		</div>
+	);
+}
+
+function Stats() {
+	return (
+		<div className="stats">
+			<span>
+				🗒️ Kamu punya x catatan dan baru x yang dichecklist (x%) ✅
+			</span>
+		</div>
+	);
 }
 
 export default App;
